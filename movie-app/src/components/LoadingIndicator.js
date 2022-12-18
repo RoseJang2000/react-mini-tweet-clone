7611165cd412b3b80598eee7d1665d32ea9ec71b
Rose Jang <jangmi749@gmail.com>
2022-12-18 21:19:01 +0900

    React movie app

---
+++ movie-app/src/components/LoadingIndicator.js
@@ -0,0 +1,12 @@
+const LoadingIndicator = () => {
+  return (
+    <img
+      className="loading-indicator"
+      alt="now loading..."
+      src="https://t1.daumcdn.net/cfile/blog/9971254B5E3A670916"
+      style={{ margin: '1rem' }}
+    />
+  );
+};
+
+export default LoadingIndicator;
